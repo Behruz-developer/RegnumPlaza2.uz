83c0af8a06b73f2372e2b9e5ce50830287a5f06f
Behruz-developer <88540036+Behruz-developer@users.noreply.github.com>
2024-08-14 22:06:44 +0500

    finish

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -22,45 +22,45 @@ const Footer = () => {
   const position = [41.307163, 69.281239];
   return (
     <footer className="footer">
-        <div className="footer_box">
+      <div className="footer_box">
         <div className="footer_card2">
-            <div className="map_box" >
-              <MapContainer
-                center={position}
-                zoom={13}
-                className="map_container"
-                // style={{ height: "100%" }}
-              >
-                <TileLayer
-                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                  opacity={0.2}
-                />
-                <TileLayer
-                  url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
-                  opacity={1}
-                />
-                <Marker position={position}>
-                  <Popup>Kosmonavtlar Metro Bekati</Popup>
-                </Marker>
-              </MapContainer>
-            </div>
-          </div>  
-          <div className="footer_card1">
-            <div className="container">
+          <div className="map_box" >
+            <MapContainer
+              center={position}
+              zoom={13}
+              className="map_container"
+            // style={{ height: "100%" }}
+            >
+              <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                opacity={0.2}
+              />
+              <TileLayer
+                url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+                opacity={1}
+              />
+              <Marker position={position}>
+                <Popup>Kosmonavtlar Metro Bekati</Popup>
+              </Marker>
+            </MapContainer>
+          </div>
+        </div>
+        <div className="footer_card1">
+          <div className="container">
             <h2 className="footer_title">Bog‘lanish</h2>
             <div className="footer_location">
               <div className="footer_location_title">
-              <FiClock   className="footer_location_icon"/>
+                <FiClock className="footer_location_icon" />
 
-              Ish vaqti
+                Ish vaqti
               </div>
               <p className="footer_location_text">
-              Har kun soat 09:00 dan 18:00 gacha
+                Har kun soat 09:00 dan 18:00 gacha
               </p>
             </div>
             <div className="footer_location">
               <div className="footer_location_title">
-              <MdOutlineLocationOn className="footer_location_icon"/>
+                <MdOutlineLocationOn className="footer_location_icon" />
 
                 Sotuv bo‘limi
               </div>
@@ -93,23 +93,16 @@ const Footer = () => {
               </div>
             </div>
             <div className="footer_card3">
-            <p className="footer_card3_text">
-              Loyiha arxitekturasi, obodonlashtirish, fasadlar, interyer
-              tasvirlari, qo'llaniladigan materiallar, ularni raqamlashtirish
-              tartibi, majmua infratuzilmasi shartli bo'lib, loyihalashtirish,
-              qurilish ishlari va foydalanishda.
-            </p>
-            <p className="footer_card3_text">
-              TM tog'risidagi ma'lumotlar ko'rsatib o' tilgan mahsulotga bolgan
-              talabni aniqlash magsadida chop etilgan. Ushbu buklet ommaviy
-              oferta sanalmaydi.
-            </p>
-          </div>
+              <p className="footer_card3_text">
+                Ushbu saytdagi suratlar loyiha arxitekturasini tushunish, fasad, dizayn va interer tasvirlari bilan tanishish, majmua infratuzilmasini oʻrganib chiqish maqsadida yuklangan. U ommaviy oferta hisoblanmaydi.
+              </p>
+
             </div>
-
           </div>
 
-          {/* <div className="footer_card3">
+        </div>
+
+        {/* <div className="footer_card3">
             <p className="footer_card3_text">
               Loyiha arxitekturasi, obodonlashtirish, fasadlar, interyer
               tasvirlari, qo'llaniladigan materiallar, ularni raqamlashtirish
@@ -122,7 +115,7 @@ const Footer = () => {
               oferta sanalmaydi.
             </p>
           </div> */}
-        </div>
+      </div>
     </footer>
   );
 };
