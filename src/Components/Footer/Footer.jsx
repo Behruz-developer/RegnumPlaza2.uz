2bf1b752d9f13e09761dc89e1905ba368e536fca
Behruz-developer <88540036+Behruz-developer@users.noreply.github.com>
2024-08-14 21:23:38 +0500

    finish

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -9,6 +9,7 @@ import "../../../node_modules/leaflet/dist/leaflet.css";
 import L from "leaflet";
 import icon from "../../assets/images/metka.png";
 import { MdOutlineLocationOn } from "react-icons/md";
+import { FiClock } from "react-icons/fi";
 const DefaultIcon = L.icon({
   iconUrl: icon,
   iconSize: [48, 56],
@@ -21,19 +22,40 @@ const Footer = () => {
   const position = [41.307163, 69.281239];
   return (
     <footer className="footer">
-      <div className="container">
         <div className="footer_box">
+        <div className="footer_card2">
+            <div className="map_box" >
+              <MapContainer
+                center={position}
+                zoom={13}
+                className="map_container"
+                // style={{ height: "100%" }}
+              >
+                <TileLayer
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                  opacity={0.2}
+                />
+                <TileLayer
+                  url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+                  opacity={1}
+                />
+                <Marker position={position}>
+                  <Popup>Kosmonavtlar Metro Bekati</Popup>
+                </Marker>
+              </MapContainer>
+            </div>
+          </div>  
           <div className="footer_card1">
+            <div className="container">
             <h2 className="footer_title">Bog‘lanish</h2>
             <div className="footer_location">
               <div className="footer_location_title">
-              <MdOutlineLocationOn className="footer_location_icon"/>
+              <FiClock   className="footer_location_icon"/>
 
-                Sotuv bo‘limi
+              Ish vaqti
               </div>
               <p className="footer_location_text">
-                Toshkent shahri, Mirobod tumani, <br /> Oybek ko‘chasi 38а,
-                Biznes center "Avalon", 1 qavat
+              Har kun soat 09:00 dan 18:00 gacha
               </p>
             </div>
             <div className="footer_location">
@@ -52,7 +74,7 @@ const Footer = () => {
                 <p className="footer_phone_text">Telefon</p>
                 <div className="footer_phone_number">
                   <BiSolidPhoneCall />
-                  1066
+                  1361
                 </div>
               </div>
               <div className="footer_icons">
@@ -70,30 +92,7 @@ const Footer = () => {
                 </a>
               </div>
             </div>
-          </div>
-          <div className="footer_card2">
-            <div >
-              <MapContainer
-                center={position}
-                zoom={13}
-                className="map_container"
-                
-              >
-                <TileLayer
-                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                  opacity={0.2}
-                />
-                <TileLayer
-                  url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
-                  opacity={1}
-                />
-                <Marker position={position}>
-                  <Popup>Kosmonavtlar Metro Bekati</Popup>
-                </Marker>
-              </MapContainer>
-            </div>
-          </div>
-          <div className="footer_card3">
+            <div className="footer_card3">
             <p className="footer_card3_text">
               Loyiha arxitekturasi, obodonlashtirish, fasadlar, interyer
               tasvirlari, qo'llaniladigan materiallar, ularni raqamlashtirish
@@ -106,8 +105,24 @@ const Footer = () => {
               oferta sanalmaydi.
             </p>
           </div>
+            </div>
+
+          </div>
+
+          {/* <div className="footer_card3">
+            <p className="footer_card3_text">
+              Loyiha arxitekturasi, obodonlashtirish, fasadlar, interyer
+              tasvirlari, qo'llaniladigan materiallar, ularni raqamlashtirish
+              tartibi, majmua infratuzilmasi shartli bo'lib, loyihalashtirish,
+              qurilish ishlari va foydalanishda.
+            </p>
+            <p className="footer_card3_text">
+              TM tog'risidagi ma'lumotlar ko'rsatib o' tilgan mahsulotga bolgan
+              talabni aniqlash magsadida chop etilgan. Ushbu buklet ommaviy
+              oferta sanalmaydi.
+            </p>
+          </div> */}
         </div>
-      </div>
     </footer>
   );
 };
